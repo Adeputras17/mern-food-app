3bc33bdebb0ff0a792e166246284fd757f14ea09
Adeputras17 <kirabelkparg@gmail.com>
2022-12-30 00:50:13 +0800

    first init

---
+++ food-client/src/components/pizzas/Pizzas.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { getAllPizzas } from '../../actions/pizzaActions'
+
+import Pizza from '../pizza/Pizza'
+import './pizzas.css'
+
+const Pizzas = () => {
+
+    const dispatch = useDispatch();
+    const pizzasstate = useSelector(state=>state.getAllPizzasReducer);
+    const {pizzas, loading, error} = pizzasstate;
+
+    useEffect(() => {
+
+        dispatch(getAllPizzas());
+
+    }, [dispatch]);
+
+  return (
+    <div className='ps-container'>
+        <div className="ps-row">
+            <div className="ps-col">
+                <div className="ps-cards">
+                    {loading ? (<h2 className='loading'>Loading...</h2>) : error ? (<h2 className='error'>Error...</h2>) : (
+                        pizzas.map((pizza) => {
+                            return (
+                                <Pizza pizza={pizza} />
+                            )
+                        })
+                    )}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Pizzas
